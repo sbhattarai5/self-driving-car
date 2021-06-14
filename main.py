8b4064859dfbff059d6db89223d6544def52d0c5
Micah <70914736+Micah0320@users.noreply.github.com>
2021-06-14 15:37:05 -0500

    Main Update
    Made the main code compatable with all the previous updates.
    Includes sensor data and collision detection, as well as classes for each object.

--- main.py
+++ main.py
@@ -1,85 +1,32 @@
-import sys
-import random; random.seed()
-import pygame
-from init import *
-from road import *
-from vehicle import *
-from clock import *
+from factory import *
 
-car_x = road_rect[0] + LANE_WIDTH/2
-car_y = 450
-car_dx = 0
-car_dy = 1
-
-audi_x = road_rect[0] + 3 * LANE_WIDTH/2
-audi_y = 500
-audi_dx = 0
-audi_dy = -0.2
-audi_angle = 0
-
-dy = 1  # check if I can comment this
-
-
-while 1:
+carmodel, carcontrol, carview, audimodel, audicontrol, audiview, roadmodel, roadcontrol, roadview, viewwindow, halter, WorldObjects = factory()
+while (1):
 
     starttime = pygame.time.get_ticks()
 
+    halter.run()
+
+    roadcontrol.run(WorldObjects)
+    audicontrol.run()
+    carcontrol.run()
+    viewwindow.run()
+
+    ########################################
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-    ## maybe
-    # variables = [car_x, car_y, audi_dx, audi_dy]
-    # manipulate_key(keys, variables)
-    ##############
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        car_x -= 0.1
-    elif keys[pygame.K_RIGHT]:
-        car_x += 0.1
-    elif keys[pygame.K_UP]:
-        dy += 0.1
-    elif keys[pygame.K_DOWN]:
-        dy -= 0.1
-    elif keys[pygame.K_r]:
-        # Test rotation
-        audi_angle = (audi_angle + 10) % 360
-        audi_image = pygame.image.load('images/small/Audi/Audi-%s.png' % audi_angle)
-            
-    for i in range(len(dividers)):
-        dividers[i].y += dy
-    if dividers[0].y >= DIVIDER_HEIGHT:
-        d = dividers.pop()
-        d.y = -DIVIDER_HEIGHT + (dividers[0].y - DIVIDER_HEIGHT)
-        dividers.insert(0, d)
-
-    # random speed change
-    car_y += dy
-    r = random.randrange(100) 
-    if r == 1:
-        audi_dy += 0.05
-    elif r == 2:
-        audi_dy -= 0.05
-        
-    audi_x, audi_y = move_vehicle(audi_x, audi_y, audi_dx, audi_dy)
-
-    # drawing
     draw_grass()
-    draw_road()
-    draw_vehicle(car_image, car_x, car_y)
-    draw_vehicle(audi_image, audi_x, audi_y)
-    draw_vehicle(mini_truck_image, 500, 200)
-    draw_vehicle(ambulance_image, 550, 200)
-    draw_vehicle(police_image, 600, 200)
-    draw_vehicle(taxi_image, 650, 200)
-    draw_vehicle(black_viper_image, 700, 200)
-    draw_vehicle(mini_van_image, 750, 200)
-    draw_clock()
+    roadview.run()
+    carmodel.sensor.detectWorld(carmodel, WorldObjects)
+    carview.run()
+    audiview.run()
     pygame.display.flip()
+
+    # create an object called FramerateControl. Replace the following codes with
+    # frameratecontrol.run()
     
-    # delaying
+    # delaying Frames. TO-DO: Add this to our halter class, very easy to do, should take 5 minutes.
     delay = get_delay(starttime)
     if delay > 0:
         pygame.time.delay(delay)
+
+    
