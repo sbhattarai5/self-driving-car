32e906f24c07ce03fbb075f89188465bb5f67b9a
Micah <70914736+Micah0320@users.noreply.github.com>
2021-06-14 15:29:38 -0500

    Halter Class
    Control for frame rates

---
+++ halter.py
@@ -0,0 +1,13 @@
+from init import *
+
+
+class Halter:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            
